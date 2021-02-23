c2c659587a14944a5bcef9c620389c6561e3e906
pedro miguel <pedro.miguel.69@hotmail.com>
2021-02-23 01:43:35 +0000

    angular fundamentals

---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+
+@Component({
+    selector: 'event-thumbnail',
+    templateUrl: './event-thumbnail.component.html',
+    styleUrls: ['./event-thumbnail.component.css'] 
+})
+export class EventThumbnailComponent {
+    @Input() event: any
+
+    getStartTimeClass() {
+        const isEarlyStart = this.event && this.event.time === '8:00 am'
+        return {green: isEarlyStart, bold: isEarlyStart}
+    }
+}
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from "@angular/core";
+import { ToastrService } from "../common/toastr.service";
+import { EventService } from "./shared/event.service";
+
+@Component({
+    selector: 'events-list',
+    templateUrl: './events-list.component.html',
+    styleUrls: ['./events-list.component.css'] 
+})
+export class EventsListComponent implements OnInit{
+    events: any[]
+    constructor(private eventService: EventService, private toastr: ToastrService) {
+
+    }
+
+    ngOnInit() {
+        this.events = this.eventService.getEvents()
+    }
+
+    handleThumbnailClick(eventName) {
+        this.toastr.success(eventName)
+    }
+
+}
